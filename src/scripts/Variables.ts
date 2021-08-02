20c3d6e82d4d1755734c1a9fb68597de68c75f20
JFortun <josemanuelfortunromero@gmail.com>
2021-08-02 12:57:12 +0200

    Variables

---
+++ src/scripts/Variables.ts
@@ -0,0 +1,11 @@
+let character: string = 'Skeleton';
+let health: number | string = 95;
+let isAlive: boolean = true;
+let skills: string[] = ['Bash', 'Counter', 'Healing'];
+
+skills.push('dash')
+
+//health = 'FULL';
+
+console.log("Name: " + character + "\nHealth: " + health + "\nAlive?: " + isAlive + "\nSkills: " + skills);
+
